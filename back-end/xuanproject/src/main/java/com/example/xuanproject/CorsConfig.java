7b2529bd9c09b8893bfab39de5c2662b8da88b86
IRAIRANOKAME\a0917 <a0977397747@gmail.com>
2025-04-11 02:50:19 +0800

    前端cros

--- back-end/xuanproject/src/main/java/com/example/xuanproject/CorsConfig.java
+++ back-end/xuanproject/src/main/java/com/example/xuanproject/CorsConfig.java
@@ -18,8 +18,10 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true); // 允許帶上 Cookie
-        config.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500", "http://localhost:5050",
-                "http://localhost:3000", "http://localhost:5000")); // 設定允許的前端來源
+        config.setAllowedOrigins(Arrays.asList(
+            "http://127.0.0.1:5500", "http://localhost:5050",
+                "http://localhost:3000", "http://localhost:5000", 
+                "http://xuan-project-2025.s3-website-ap-northeast-1.amazonaws.com")); // 設定允許的前端來源
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 允許的 HTTP 方法
         config.setAllowedHeaders(Arrays.asList("*")); // 允許所有標頭
 
