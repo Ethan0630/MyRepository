5bc7bde6a9d20ec2f449943898296b592c2d9c34
IRAIRANOKAME\a0917 <a0977397747@gmail.com>
2025-04-11 21:18:41 +0800

    登入旁邊的豬豬

--- front-end/demoFrontend0301/my-react-app/src/Login.jsx
+++ front-end/demoFrontend0301/my-react-app/src/Login.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Login.css'; // 建議把樣式分離寫這個檔案
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPiggyBank } from '@fortawesome/free-solid-svg-icons';
 
 const Login = () => {
     const [username, setUsername] = useState('');
@@ -44,16 +46,17 @@ const Login = () => {
     return (
         <div id="login-container">
             <div id="title">
-                <h3><i className="fa-solid fa-piggy-bank" onClick={generate}></i>登入</h3>
+                <FontAwesomeIcon icon={faPiggyBank} className="piggy-icon" onClick={generate}/>
+                <h3 >登入</h3>  
             </div>
 
             <div className="container">
-                <span>*帳號</span>
+                <span>*帳號:</span>
                 <input type="text" placeholder="輸入帳號" value={username} onChange={(e) => setUsername(e.target.value)} />
             </div>
 
             <div className="container">
-                <span>*密碼</span>
+                <span>*密碼:</span>
                 <input type="password" placeholder="輸入密碼" value={password} onChange={(e) => setPassword(e.target.value)} />
             </div>
 
